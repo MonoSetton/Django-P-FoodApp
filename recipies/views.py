6e14c761d87e13b90964a7e65caa19da90125a11
MonoSetton <maksymiliankloch@gmail.com>
2023-07-28 16:48:53 +0200

    Showing recipies on the home page

--- recipies/views.py
+++ recipies/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
+import requests
+
+
+api_key = '99aa443bda864d698ad5ac6db226c843'
 
 
 def home(request):
-    context = {}
+    titles = []
+    images = []
+    url = "https://api.spoonacular.com/recipes/random"
+    r = requests.get(f'{url}?tags=lunch&number=9&apiKey={api_key}')
+    r = r.json()
+    r = r['recipes']
+    for index, item in enumerate(r):
+        titles.append(r[index]['title'])
+        if 'image' in r[index]:
+            images.append(r[index]['image'])
+        else:
+            images.append()
+    items = zip(titles, images)
+    context = {'items': items}
     return render(request, 'recipies/home.html', context)
 
 
