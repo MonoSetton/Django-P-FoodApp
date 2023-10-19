6c8e210f3c646919403f666442ff91823a18417c
MonoSetton <maksymiliankloch@gmail.com>
2023-10-19 22:38:36 +0200

    Fixed insert requirements

--- recipes/views.py
+++ recipes/views.py
@@ -67,20 +67,20 @@ def requirements_recipes(request):
                 query += f'&{key}={value}'
         try:
             r = requests.get(f'{url}/findByNutrients?apiKey={api_key}&number=9&random=true{query}').json()
-        except:
-            nutrients = Nutrient.objects.all()
-            error_message = 'Something in Your choices is not right. Please try again.'
-            context = {'nutrients': nutrients, 'error_message': error_message}
-            return render(request, 'recipes/insert_requirements.html', context)
-        else:
-
             for index, item in enumerate(r):
                 titles.append(r[index]['title'])
                 images.append(r[index]['image'])
                 ids.append(r[index]['id'])
             items = zip(titles, images, ids)
+        except (KeyError, ValueError):
+            nutrients = Nutrient.objects.all()
+            error_message = 'Something in Your choices is not right. Please try again.'
+            context = {'nutrients': nutrients, 'error_message': error_message}
+            return render(request, 'recipes/insert_requirements.html', context)
+        else:
             context = {'items': items}
             return render(request, 'recipes/recipes_from_requirements.html', context)
+
     else:
         nutrients = Nutrient.objects.all()
         context = {'nutrients': nutrients}
