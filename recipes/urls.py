c348be0b4af9ae27a264f782174f277728b6060d
MonoSetton <maksymiliankloch@gmail.com>
2023-10-19 22:14:42 +0200

    Fix

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.home, name='home'),
+    path('', views.home, name='home'),
     path('detail_recipes/<str:pk>', views.detail_recipes, name='detail_recipes'),
     path('insert_ingredients', views.ingredients_recipes, name='insert_ingredients'),
     path('insert_requirements', views.requirements_recipes, name='insert_requirements'),
