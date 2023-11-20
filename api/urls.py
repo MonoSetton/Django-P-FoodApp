5b329b60881bff1f616782617bb7d0e0e30a11b0
MonoSetton <maksymiliankloch@gmail.com>
2023-11-20 15:20:35 +0100

    Added refreshing reipes after provideing ingredients

--- api/urls.py
+++ api/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.api_overview, name='api'),
     path('recipes-random/', views.recipes_random, name='recipes-random'),
     path('nutrients-list/', views.nutrients_list, name='nutrients-list'),
+    path('recipes-ingredients/', views.recipes_ingredients, name='recipes-ingredients'),
 ]
--- api/views.py
+++ api/views.py
@@ -2,7 +2,6 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import NutrientSerializer
 from recipes.models import Nutrient, ForeignAPI
-from recipes.forms import InsertIngredients
 import requests
 
 
@@ -16,6 +15,7 @@ def api_overview(request):
     api_urls = {
         'Random Recipes': 'recipes-random',
         'List of Nutrients': 'nutrients-list',
+        'Recipes from Ingredients': 'recipes-ingredients',
     }
     return Response(api_urls)
 
@@ -42,8 +42,25 @@ def recipes_random(request):
         else:
             images.append('image_not_found')
     items = zip(titles, images, ids, readyInMinutes, servings)
-    form = InsertIngredients()
-    context = {'items': items, 'form': form}
+    return Response(items)
+
+
+@api_view(['POST'])
+def recipes_ingredients(request):
+    titles, images, ids = [], [], []
+    conditionals = 'number=2&ranking=1&ignorePantry=false'
+    ingredients = request.data.get('ingredients')
+    r = requests.get(f'{url}/findByIngredients?apiKey={api_key}&'
+                     f'{conditionals}&'
+                     f'ingredients={ingredients}').json()
+    for index, item in enumerate(r):
+        titles.append(r[index]['title'])
+        ids.append(r[index]['id'])
+        if 'image' in r[index]:
+            images.append(r[index]['image'])
+        else:
+            images.append('image_not_found')
+    items = zip(titles, images, ids)
     return Response(items)
 
 
