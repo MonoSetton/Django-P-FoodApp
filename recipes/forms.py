b0b79775a5288cbd840ccce2618e8141860a61c1
MonoSetton <maksymiliankloch@gmail.com>
2024-02-10 15:38:49 +0100

    Cleanded Code

--- recipes/forms.py
+++ recipes/forms.py
@@ -27,9 +27,9 @@ class StepForm(forms.ModelForm):
 
 
 IngredientFormSet = inlineformset_factory(
-    CustomRecipe, Ingredient, form=IngredientForm, extra=2, can_delete=False
+    CustomRecipe, Ingredient, form=IngredientForm, extra=6, can_delete=False
 )
 
 StepFormSet = inlineformset_factory(
-    CustomRecipe, Step, form=StepForm, extra=2, can_delete=False
+    CustomRecipe, Step, form=StepForm, extra=8, can_delete=False
 )
